12822e2af77c275106e75d9de2908a28726a78dc
rtleib <rtleib@gmail.com>
2022-03-06 16:37:37 -0600

    updating in github

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 var timerEl = document.getElementById("countdown")
 var mainEl = document.getElementById("main")
+var startButton = document.getElementById("start-button")
+var questionText = document.querySelector(".questions")
+var answerAText = document.getElementById("answerA")
+var answerBText = document.getElementById("answerB")
+var answerCText = document.getElementById("answerC")
+var answerDText = document.getElementById("answerD")
 
-countdown();
+
 function countdown() {
     var timeLeft = 75;
     var timeInterval = setInterval(function(){
@@ -12,3 +18,19 @@ function countdown() {
         }
     }, 1000);
 }
+countdown();
+
+function generateQuiz(questions){
+}
+
+function showQuestions(questions){}
+var questionsArr = [
+    {
+        question: "testing",
+        answerA: "testAnswer"
+    }
+]
+function quizInfo() {
+    questionText.textContent = questionsArr[0].question
+}
+startButton.addEventListener("click", quizInfo);
